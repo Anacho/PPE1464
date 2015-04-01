daf61a11b64766ba5ed2ea75fc415f108a6cb3da
JBFavrot <jeanbaptiste.favrot@outlook.fr>
2015-04-01 17:13:04 +0200

    IA Fonctionnelle
    L'IA construit automatiquement des tourelles le long du chemin de
    l'ennemi

--- SpacePirates/Assets/_scripts/IA.cs
+++ SpacePirates/Assets/_scripts/IA.cs
@@ -5,17 +5,18 @@ using System.Collections.Generic;
 
 public class IA : MonoBehaviour {
 
-	private List<Controller.Coord> listCoords = new List<Controller.Coord>(Controller.ArrayCoordOpponent);
+	private List<Controller.Coord> listCoords = Controller.ArrayCoordOpponent;
 	private List<Controller.Coord> listToBuild = new List<Controller.Coord>();
 	
+	public Tower towerPrefab = null;
 
+	float timeLeft = 0.0f;
+	float interval = 0.8f;
 
 	// Use this for initialization
 	void Start () 
 	{
-
-		bool duplicate;
-
+		Debug.Log("Début de l'initialisation");
 		foreach(Controller.Coord coord in listCoords) 
 		{
 			Controller.Coord[] coords = new Controller.Coord[4];
@@ -30,26 +31,56 @@ public class IA : MonoBehaviour {
 
 			foreach(Controller.Coord tempCoord in coords)
 			{
+
 				if(tempCoord.x<Controller.MapSizeX && tempCoord.x>=0 && tempCoord.y<Controller.MapSizeY && tempCoord.y>=0)
 				{
 					if(!listCoords.Contains(tempCoord))
 					{
-						if(!listToBuild.Contains(tempCoord)) listToBuild.Add (tempCoord);
+						if(!listToBuild.Contains(tempCoord)) 
+						{
+							if(!(tempCoord.x==((Controller.MapSizeX-1)/2)&&(tempCoord.x==((Controller.MapSizeX-1)/2))))
+							{
+								listToBuild.Add (tempCoord);
+							}
+						}
 					}
 				}
 			}
+			foreach(Controller.Coord coordonnees in listToBuild)
+			{
+				//Debug.Log("x: "+coordonnees.x + " y: "+coordonnees.y);
+			}
+
 		}
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Castle.enemyGold>0) 
+		timeLeft -= Time.deltaTime;
+		if (timeLeft <= 0.0f) 
 		{
-			int rnd;
-			rnd = (int)(Random.Range(0, listToBuild.Count));
-			
+			if(listToBuild.Count>0)
+			{
+				if (Castle.enemyGold > 0) {
+					int rnd;
+					rnd = (int)(Random.Range (0, listToBuild.Count));
+					Debug.Log("x: "+listToBuild[rnd].x + " y: "+listToBuild[rnd].y);
+					BuildTurret (listToBuild [rnd], true);
+					listToBuild.RemoveAt(rnd);
+					Castle.enemyGold--;
+				}
+			}
+			timeLeft=interval;
 		}
+
+	}
+
+	void BuildTurret(Controller.Coord coord, bool enemy)
+	{
+		Tower tower = (Tower) Instantiate (towerPrefab, Controller.MapOpponent[coord.x, coord.y].transform.position, new Quaternion ());
+		tower.enemy = enemy;
 	}
 }

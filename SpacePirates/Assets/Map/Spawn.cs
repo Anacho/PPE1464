523b8ca30f51bdb94e5d1c7dacbef3ef779e5f7b
tischenbach <romain.tischenbach@hotmail.fr>
2015-04-06 15:14:37 +0200

    vague
    vague

--- SpacePirates/Assets/Map/Spawn.cs
+++ SpacePirates/Assets/Map/Spawn.cs
@@ -5,25 +5,63 @@ public class Spawn : MonoBehaviour {
 
 
 	public bool enemy = false;
+	static int nbRobot=0;
+	int prixRobot=1;
+	int vague = 0;
+	bool newVague=true;
+	string text="Unité\n\nUnités de base : 5 robots\nAjout d'unités : ";
 	
-	/*bool gui = false;
+	protected bool gui = false;
+	bool guiButton=true;
 	void OnGUI() {
-		if (gui) {
-			GUI.skin = skin;
-			// creation menu for tower
-			int width = 200;
-			int height = 40;
-			Rect r = new Rect(Screen.width/2 - width / 2, Screen.height/2 - height / 2, width, height);
-			GUI.contentColor = (Color.green);
-			GUI.Box(r,"Victory!");
+		if(!enemy){
+			GUI.Label(new Rect(200,0,100,100),"Vague "+vague);
+			GUI.skin.button.fontSize = (int)(0.08*Screen.height);
+			if(guiButton){
+				if (GUI.Button(new Rect(0,(float)(Screen.height-0.1*Screen.height),(float)(0.12*Screen.width),(float)(0.1*Screen.height)),"Unité")){
+					guiButton=false;
+					gui=true;
+				}
+			}
+			
+			if (gui) {
+				Rect r = new Rect( 0, 0, Screen.width, Screen.height);
+				GUI.Box(r, text + nbRobot);
+				
+				GUI.Label(new Rect(80,150,100,100),"Robot(s) : "+nbRobot);
+				GUI.Label(new Rect(80,170,100,100),"Prix : "+prixRobot);
+				
+				if (GUI.Button(new Rect(50,200,50,50),"-")){
+					if(nbRobot>0){
+						nbRobot--;
+						Castle.gold+=prixRobot;
+					}
+				}
+				
+				if (GUI.Button(new Rect(120,200,50,50),"+")){
+					if(Castle.gold>=+prixRobot){
+						nbRobot++;
+						Castle.gold-=prixRobot;
+					}
+				}
+				
+				
+				if (GUI.Button(new Rect(200,(float)(Screen.height-0.1*Screen.height),(float)(0.12*Screen.width),(float)(0.1*Screen.height)),"Retour")){
+					guiButton=true;
+					gui=false;
+				}
+			}
 		}
-	}*/
+	}
+
 
 
 	// spawn a new teddy each ... seconds
 	public float interval = 3.0f;
+	public float intervalVague=10.0f;
+	float timeLeftVague = 0.0f;
 	float timeLeft = 0.0f;
-	public int count = 10;
+	public int count = 0;
 
 
 
@@ -36,16 +74,7 @@ public class Spawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!Game.done && !Controller.bMapEditor) {
-			/*if (count == 0) {
-				
-				Unite[] unites = (Unite[])FindObjectsOfType(typeof(Unite));
-				
-				
-				if(unites.Length==0){
-					//gui=true;
-					Game.done=true;
-				}
-			}*/
+
 			// time to spawn the next one?
 			timeLeft -= Time.deltaTime;
 			if (timeLeft <= 0.0f) {
@@ -70,6 +99,17 @@ public class Spawn : MonoBehaviour {
 					// reset time
 					timeLeft = interval;
 
+			}
+
+
+			if(count==0){
+				timeLeftVague-=Time.deltaTime;
+				if(timeLeftVague <= 0.0f){
+					count=5+nbRobot;
+					nbRobot=0;
+					vague++;
+					timeLeftVague=intervalVague;
+				}
 			}
 		}
 	}
